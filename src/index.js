bd29927707e5e47ab4c6c40c7cd2f86486c4b1b9
oabanin-WORK <juristaba@gmail.com>
2020-03-17 16:42:45 +0400

    redux counter without Ret React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//const initialState = 0;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state = 0, action) => {
+
+    // if(state === undefined) {
+    //     return 0;
+    // }
+
+
+    // if(action.type === 'INC'){
+    //     return state + 1;
+    // }
+
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+
+        case 'DEC':
+            return state - 1;
+
+        default:
+            return state;
+    }
+};
+
+// let state = reducer(undefined, {});
+
+// state = reducer(state, {type : 'INC'});
+// state = reducer(state, {type : 'INC'});
+// console.log(state);
+
+const store = createStore(reducer);
+
+
+// store.subscribe(()=>{
+//     console.log(store.getState()) 
+// })
+
+
+//console.log(store.getState())
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'INC'})
+//console.log(store.getState())
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch({ type: 'DEC' })
+});
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch({ type: 'INC' })
+});
+
+const update = () => {
+    document
+        .getElementById('counter')
+        .innerHTML = store.getState()
+};
+
+store.subscribe(update);
