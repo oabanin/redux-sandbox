a9470ab614a88b89f8047215552719313094cdd1
oabanin-WORK <juristaba@gmail.com>
2020-03-18 16:27:14 +0400

    add binding to actions creators

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import { createStore, bindActionCreators } from 'redux';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions';
+
 import reducer from './reducer';
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
-const incDispatch = bindActionCreators(inc, dispatch)
-const decDispatch = bindActionCreators(dec, dispatch)
-const rndDispatch = bindActionCreators(rnd, dispatch)
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch)
 
-document.getElementById('dec').addEventListener('click', decDispatch);
-document.getElementById('inc').addEventListener('click', incDispatch);
 
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('inc').addEventListener('click', inc);
 document.getElementById('rnd').addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-    rndDispatch(payload);
+    rnd(payload);
 });
 
 const update = () => {
