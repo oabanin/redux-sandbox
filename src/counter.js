0c4810fc4f8cbd0bf188e8c85fbb521e85ef89e9
oabanin-WORK <juristaba@gmail.com>
2020-03-18 17:18:31 +0400

    add react component Counter without update function

---
+++ src/counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = ({counter, dec, inc, rnd}) => {
+    return (
+        <div class="jumbotron">
+            <h2>{counter}</h2>
+            <button onClick={dec} className="btn btn-primary btn-large">DEC</button>
+            <button onClick={inc} className="btn btn-primary btn-large">INC</button>
+            <button onClick={rnd} className="btn btn-primary btn-large">RND</button>
+        </div>)
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './counter';
+
 import { createStore, bindActionCreators } from 'redux';
 import * as actions from './actions';
 
@@ -9,17 +13,26 @@ const { dispatch } = store;
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch)
 
 
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('rnd').addEventListener('click', () => {
-    const payload = Math.floor(Math.random() * 10);
-    rnd(payload);
-});
+// document.getElementById('dec').addEventListener('click', dec);
+// document.getElementById('inc').addEventListener('click', inc);
+// document.getElementById('rnd').addEventListener('click', () => {
+//     const payload = Math.floor(Math.random() * 10);
+//     rnd(payload);
+// });
 
 const update = () => {
-    document
-        .getElementById('counter')
-        .innerHTML = store.getState()
+    // document
+    //     .getElementById('counter')
+    //     .innerHTML = store.getState()
 };
 
 store.subscribe(update);
+
+ReactDOM.render (<Counter 
+    counter={store.getState()}
+    inc={inc}
+    dec={dec}
+    rnd={()=>{
+    const payload = Math.floor(Math.random() * 10);
+    rnd(payload);
+    }}/>, document.getElementById('root'));
